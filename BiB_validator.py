67917cddfbf3142f2f52b6a7b7ea99891514c44b
a-wyrm <86126872+a-wyrm@users.noreply.github.com>
2024-02-17 18:40:08 -0500

    edit

--- BiB_validator.py
+++ BiB_validator.py
@@ -1,5 +1,4 @@
 from pybtex.database import BibliographyData, parse_file
-from collections import OrderedDict
 
 def remove_abstracts(bib):
   """Remove abstracts from bib."""
@@ -25,54 +24,13 @@ def remove_file(bib):
 # Taken from Max Golla's bib validator
 # https://github.com/m33x/MaxBib/blob/master/order_within.py
 
-def readfile(filename):
-    result = []
-    with open(filename, 'r', encoding="utf8") as inputfile:
-        for line in inputfile:
-            line = line.rstrip('\r\n')
-            result.append(line)
-    return result
-
-
-def get_bibitems(bib):
-    """Where does a bibentry start, where does it end?"""
-    result = OrderedDict()
-    item = []
-    entry_name = ''
-    for line in bib:
-        if line != '':
-            item.append(line)
-        if line.startswith('@'):
-            entry_name = line
-        if line.startswith('}'):
-            result[entry_name] = item
-            entry_name = ''
-            item = []
-    return result
-
-def convert_to_bibentries(maxbib_items):
-    entries = OrderedDict()
-    for item in maxbib_items:
-        entry = OrderedDict()
-        entries[item] = entry
-    return entries
-
-def enforce_order(name, entry, order):
-  """Parse the bib item to something meaningful, aka identify title, author, year etc."""
-  for k, v in enumerate(entry):
-    if v != order[k]:
-        print(name)
-        print("Wrong order", v, "!=",order[k])
-        print(entry)
-        print()
-
-
 
 def main():
     # change file name for different file
-    file = "./BIB Files/Accessible Security and Privacy/Visual Impairment/Visual Impairment.bib"
+    file = "./BIB Files/Accessibility/Accessibility.bib"
     remove_abstracts(file)
     remove_file(file)
+
             
             
 
